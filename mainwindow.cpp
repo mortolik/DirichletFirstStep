07ae92229d2d9c7214df37e2a479ce503a8976ac
Anatoliy Morozov <anatoly.morozov@radiogigabit.net>
2025-05-13 19:34:10 +0300

    Rewrite mainwindow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,8 +18,8 @@ MainWindow::MainWindow(QWidget *parent)
 
 void MainWindow::setupTestProblem()
 {
-    int n = 100;
-    int m = 100;
+    int n = 50;
+    int m = 50;
     double omega = 1.8;
     double eps = 1e-6;
     int maxIter = 10000;
@@ -27,18 +27,8 @@ void MainWindow::setupTestProblem()
     m_testModel->setup(n, m, omega, eps, maxIter);
     m_testModel->solveTestProblem();
 
-    // Создаём внутренний QTabWidget
-    QTabWidget *testTabs = new QTabWidget;
-
-    QString rep = m_testModel->reportString(true);
-    auto *vTable = new DirichletDisplayWidget(m_testModel, true, this);
-    testTabs->addTab(vTable, "v(x,y) + таблица");
-
-    // Uncomment when need error widget
-    // auto *errorWidget = new DirichletWidget(new QVector<QVector<double>>(m_testModel->error()), 1.0, 2.0, 2.0, 3.0, this);
-    // testTabs->addTab(errorWidget, "Ошибка |u* - v|");
-
-    m_tabWidget->addTab(testTabs, "Тестовая задача");
+    auto *display = new DirichletDisplayWidget(m_testModel, true, this);
+    m_tabWidget->addTab(display, "Тестовая задача");
 }
 
 void MainWindow::setupMainProblem()
@@ -52,22 +42,8 @@ void MainWindow::setupMainProblem()
     m_mainModel->setup(n, m, omega, eps, maxIter);
     m_mainModel->solveMainProblem();
 
-    // Вкладка 1: численное решение
-    DirichletWidget *mainSol = new DirichletWidget(m_mainModel, false, this);
-
-    // Вкладка 2: сравнение с сеткой 2n × 2m
-    double eps2;
-    QVector<QVector<double>> diff = m_mainModel->compareWithFinerGrid(n * 2, m * 2, eps2);
-
-    QString accReport = m_mainModel->reportString(false, eps2);
-    auto *accuracyWidget = new  DirichletWidget(m_mainModel, false, this);
-
-
-    QTabWidget *mainTabs = new QTabWidget;
-    mainTabs->addTab(mainSol, "Численное решение");
-    mainTabs->addTab(accuracyWidget, QString("Разность с сеткой 2n×2m (ε₂ ≈ %1)").arg(eps2, 0, 'e', 2));
-
-    m_tabWidget->addTab(mainTabs, "Основная задача");
+    auto *display = new DirichletDisplayWidget(m_mainModel, false, this);
+    m_tabWidget->addTab(display, "Основная задача");
 }
 
 
